283cd2efb6d648778f1b2f6a1425e07740eef402
Tulio-del <tuliogabriel425@gmail.com>
2022-07-23 17:44:34 -0300

    Primeiro commit

---
+++ desafio2.ts
@@ -0,0 +1,53 @@
+// Como podemos melhorar o esse código usando TS? 
+
+/*let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}
+*/
+
+enum Trabalho{ 
+    atriz,
+    Padeiro
+}
+
+type Humano={
+    nome:string;
+    idade:number;
+    profissao:Trabalho;
+}
+
+let  pessoa2 :Humano={
+    nome:'roberto',
+    idade:19,
+    profissao:Trabalho.Padeiro,
+}
+
+let  pessoa3 :Humano={
+    nome:'laura',
+    idade:19,
+    profissao:Trabalho.atriz,
+}
+
+let  pessoa4 :Humano={
+    nome:'carlos',
+    idade:19,
+    profissao:Trabalho.Padeiro,
+}
